d65377d1be6a2818a4210799b5d3eb4a38134ea1
Solveig Bjørkholt <39405720+sbjorkholt@users.noreply.github.com>
2021-09-24 08:33:48 +0200

    Update Paneldata_seminar_12_14.R

--- fordypningsseminar2/Paneldata_seminar_12_14.R
+++ fordypningsseminar2/Paneldata_seminar_12_14.R
@@ -4,7 +4,7 @@
 ###########################
 
 # 1. Ingen utelatt variabelskjevhet
-# 2. Lineær sammenheng mellom variablene
+# 2. LineÃ¦r sammenheng mellom variablene
 # 3. Ingen autokorrelasjon/Uavhengige observasjoner
 # 4. Normalfordelte residualer
 # 5. Homoskedastiske residualer
@@ -16,23 +16,23 @@
 ####################################
 
 
-library(haven) # For å kunne lese inn .dta-filer
-library(tidyverse) # For å kunne bruke ggplot, dplyr og liknende
-library(stargazer) # For å kunne lage pene tabeller
+library(haven) # For Ã¥ kunne lese inn .dta-filer
+library(tidyverse) # For Ã¥ kunne bruke ggplot, dplyr og liknende
+library(stargazer) # For Ã¥ kunne lage pene tabeller
 
 # Laster inn data
 data <- read_dta("neumayer_spess_2005.dta")
 
 ## I paneldata har vi observasjoner for flere enheter over tid.
-## Typisk for land og for år.
+## Typisk for land og for Ã¥r.
 
 glimpse(data)
 
 
 #### Hvorfor bruke paneldata? ####
 
-## 1. Flere observasjoner, større N, mer informasjon.
-## 2. Mulighet for å kontrollere for årsakssammenheng (årsaksretning): OVB!
+## 1. Flere observasjoner, stÃ¸rre N, mer informasjon.
+## 2. Mulighet for Ã¥ kontrollere for Ã¥rsakssammenheng (Ã¥rsaksretning): OVB!
 
 library(plotly)
 
@@ -48,7 +48,7 @@ ggplotly(time)
 
 library(plm) # Vi bruker plm-pakken
 
-data_plm <- pdata.frame(data, index = c("country", "year")) # Må opprette et plm-objekt
+data_plm <- pdata.frame(data, index = c("country", "year")) # MÃ¥ opprette et plm-objekt
 
 class(data_plm) # pdata.frame
 
@@ -67,7 +67,7 @@ head(attr(data_plm, "index"))
 # Er dataene balansert?
 
 is.pbalanced(data_plm)
-## Like mange land-variabler som år-variabler?
+## Like mange land-variabler som Ã¥r-variabler?
 ## Tar ikke hensyn til missing
 
 data.complete <- data_plm %>% 
@@ -75,27 +75,27 @@ data.complete <- data_plm %>%
   mutate(year = droplevels(year), 
          country = droplevels(country))
 
-# Er dataene balansert etter å ha tatt ut missing?
+# Er dataene balansert etter Ã¥ ha tatt ut missing?
 is.pbalanced(data.complete)
 
-# Skal vi gjøre data balansert?
-## Fordelen er at dette gir bedre prediksjonskraft på modellen (vi får ikke skjevhet).
-## Ulempen er at vi ofte mangler god nok informasjon til å skape balanse.
+# Skal vi gjÃ¸re data balansert?
+## Fordelen er at dette gir bedre prediksjonskraft pÃ¥ modellen (vi fÃ¥r ikke skjevhet).
+## Ulempen er at vi ofte mangler god nok informasjon til Ã¥ skape balanse.
 
 data.balanced.time <- make.pbalanced(data.complete, 
                                      balance.type = "shared.times") 
-# Beholder bare de enhetene (landene) der vi har data for alle tidsperiodene (årene)
+# Beholder bare de enhetene (landene) der vi har data for alle tidsperiodene (Ã¥rene)
 # Dette datasettet inneholder 0 observasjoner fordi vi ikke har data for alle tidsperioder for noen enheter
 
 glimpse(data.balanced.time)
 
-# Beholder bare de tidsperiodene (årene) der vi har data for alle enhetene (landene)
+# Beholder bare de tidsperiodene (Ã¥rene) der vi har data for alle enhetene (landene)
 data.balanced.ind <- make.pbalanced(data.complete, 
                                     balance.type = "shared.individuals") %>%  
   mutate(year = droplevels(year),                               
          country = droplevels(country)) 
-# Fjerner faktornivåene (levels) til de årene og landene som ikke 
-# lengre er med i datasettet for å unngå følgefeil
+# Fjerner faktornivÃ¥ene (levels) til de Ã¥rene og landene som ikke 
+# lengre er med i datasettet for Ã¥ unngÃ¥ fÃ¸lgefeil
 
 glimpse(data.balanced.ind)
 
@@ -107,12 +107,12 @@ length(unique(data.balanced.ind$country))
 
 length(unique(data.complete$year))
 length(unique(data.balanced.ind$year))
-# Vi har fortsatt data for 32 år
-# Dette gir mening fordi vi valgte å beholde de enhetene vi hadde observasjoner for alle år for. 
+# Vi har fortsatt data for 32 Ã¥r
+# Dette gir mening fordi vi valgte Ã¥ beholde de enhetene vi hadde observasjoner for alle Ã¥r for. 
 
 
 
-#### Hva slags modell kan vi kjøre med paneldata? ####
+#### Hva slags modell kan vi kjÃ¸re med paneldata? ####
 
 # Pooled
 # Fixed effects
@@ -121,7 +121,7 @@ length(unique(data.balanced.ind$year))
 
 #### Pooled oLS ####
 
-# I plm får vi en vanlig OLS-modell om vi velger model = "pooling". 
+# I plm fÃ¥r vi en vanlig OLS-modell om vi velger model = "pooling". 
 mod1ols <- plm(data = data.complete, 
                fdi_inflow ~ bits + ln_gdp_pr_cap + ln_population +
                  economic_growth + inflation + resource_rent + 
@@ -144,8 +144,8 @@ data.complete <- data.complete %>%
 # Eye ball test av heteroskedastisitet
 ggplot(data.complete %>% 
          filter(country %in% c("Norway", "Ethiopia", "Chile", "Estonia",
-                               "Chad", "Switzerland", "Spain")) %>% # Velger noen land som jeg plotter for å ikke få et helt uoversiktelig plot
-         data.frame(),# Gjør om til en data.frame objekt for å plotte
+                               "Chad", "Switzerland", "Spain")) %>% # Velger noen land som jeg plotter for Ã¥ ikke fÃ¥ et helt uoversiktelig plot
+         data.frame(),# GjÃ¸r om til en data.frame objekt for Ã¥ plotte
        aes(x = fdi_inflow_pred, y = resid)) +         
   geom_point(aes(col = country)) +
   geom_smooth(method = "lm")
@@ -153,7 +153,7 @@ ggplot(data.complete %>%
 
 # Utfordring 2: Samtidig korrelasjon
 
-### Løsning: Vi må ha panelkorrigerte standardfeil: PCSE
+### LÃ¸sning: Vi mÃ¥ ha panelkorrigerte standardfeil: PCSE
 # vcovBK gir panelkorrigerte standardfeil
 
 # Beregner PCSE:
@@ -165,18 +165,18 @@ stargazer(mod1ols, mod1ols, type = "text",
           column.labels = c("Med PCSE", "Med vanlige SE"),
           se = list(bkse))
 
-## Retter opp i både heteroskedastisitet og samtidig korrelasjon
+## Retter opp i bÃ¥de heteroskedastisitet og samtidig korrelasjon
 
 
 # Utfordring 3: Autokorrelasjon
 
-pdwtest(mod1ols) # Sjekker autokorrelasjonen (nærhet av 0 = autokorrelasjon (mot nærhet mot 2))
+pdwtest(mod1ols) # Sjekker autokorrelasjonen (nÃ¦rhet av 0 = autokorrelasjon (mot nÃ¦rhet mot 2))
 
-# Løsninger: Lagget avhengig variabel eller differensiering
+# LÃ¸sninger: Lagget avhengig variabel eller differensiering
 
-# Kjører modellen med lagget avhengig variabel som uavhengig variabel 
+# KjÃ¸rer modellen med lagget avhengig variabel som uavhengig variabel 
 mod1ols_lag <- plm(data = data.complete, 
-                   fdi_inflow ~ lag(fdi_inflow, 1) +  # Bytt ut "1" med ønsket antall lags dersom du vil ha mer enn 1
+                   fdi_inflow ~ lag(fdi_inflow, 1) +  # Bytt ut "1" med Ã¸nsket antall lags dersom du vil ha mer enn 1
                      bits + ln_gdp_pr_cap + ln_population +
                      economic_growth + inflation + resource_rent + 
                      bilateral_trade_agreements + wto_member + polcon3,
@@ -185,14 +185,14 @@ mod1ols_lag <- plm(data = data.complete,
 plm::pdwtest(mod1ols_lag) # p > 0.05 og DW = 2.29
 
 
-# Vi kan også differensiere
-# Her lager jeg først en variabel som er lik Yt - Yt-1
+# Vi kan ogsÃ¥ differensiere
+# Her lager jeg fÃ¸rst en variabel som er lik Yt - Yt-1
 data.complete$y.lag <- lag(data.complete$fdi_inflow, 1)
 data.complete$y_diff <- data.complete$fdi_inflow - data.complete$y.lag
 
-# Kjører modellen med differensiert avhengig variabel for å illustrere hvordan det kan gjøres
+# KjÃ¸rer modellen med differensiert avhengig variabel som uavhengig variabel for Ã¥ illustrere hvordan det kan gjÃ¸res
 mod1ols_diff <- plm(data = data.complete, 
-                    fdi_inflow ~ y_diff + # Bytt ut "1" med ønsker antall lags dersom du vil ha mer enn 1
+                    fdi_inflow ~ y_diff + # Bytt ut "1" med Ã¸nsker antall lags dersom du vil ha mer enn 1
                       bits + ln_gdp_pr_cap + ln_population +
                       economic_growth + inflation + resource_rent + 
                       bilateral_trade_agreements + wto_member + polcon3,
@@ -203,7 +203,7 @@ summary(mod1ols_diff)
 plm::pdwtest(mod1ols_diff) # p < 0.05 og DW = 0.37
 # autokorrelasjon
 
-## Det finnes også robuste standardfeil mot autokorrelasjon i residual
+## Det finnes ogsÃ¥ robuste standardfeil mot autokorrelasjon i residual
 
 stargazer(mod1ols, mod1ols_lag, mod1ols_diff, 
           type = "text",
@@ -212,12 +212,12 @@ stargazer(mod1ols, mod1ols_lag, mod1ols_diff,
 
 #### Fixed effects ####
 # Kontrollerer for det som er fast over tid (f. eks. kultur)
-## Kontrollerer for ting som er vanskelig å fange opp
+## Kontrollerer for ting som er vanskelig Ã¥ fange opp
 ## Uheldig hvis det som er fast over tid varierer mellom enheter
 ## Bias ved korte tidsserier ( < 20)
 
 # Med tversnittsfaste effekter (i dette tilfellet land)
-# Kontrollerer ut variasjon mellom land, undersøker bare variasjon innad i landet.
+# Kontrollerer ut variasjon mellom land, undersÃ¸ker bare variasjon innad i landet.
 plm.fe.ind <- plm(data = data.complete, 
                   fdi_inflow ~ bits + ln_gdp_pr_cap + ln_population +
                     economic_growth + inflation + resource_rent + 
@@ -225,15 +225,15 @@ plm.fe.ind <- plm(data = data.complete,
                   na.action = "na.exclude", model = "within", effect = "individual")
 
 
-# Med tidsfaste effekter (i dette tilfellet år)
-# Kontrollerer ut variasjon mellom år, undersøker bare variasjon per år.
+# Med tidsfaste effekter (i dette tilfellet Ã¥r)
+# Kontrollerer ut variasjon mellom Ã¥r, undersÃ¸ker bare variasjon per Ã¥r.
 plm.fe.time <- plm(data = data.complete, 
                    fdi_inflow ~ bits + ln_gdp_pr_cap + ln_population +
                      economic_growth + inflation + resource_rent + 
                      bilateral_trade_agreements + wto_member + polcon3,
                    na.action = "na.exclude",model = "within", effect = "time")
 
-# Med tversnitts- og tidsfaste effekter (i dette tilfellet år og land)
+# Med tversnitts- og tidsfaste effekter (i dette tilfellet Ã¥r og land)
 plm.fe.two <- plm(data = data.complete, 
                   fdi_inflow ~ bits + ln_gdp_pr_cap + ln_population +
                     economic_growth + inflation + resource_rent + 
@@ -246,19 +246,19 @@ stargazer(plm.fe.ind, plm.fe.time, plm.fe.two, type = "text",
           column.labels = c("Tversnitts FE", "Tids FE", "Tversnitts og tids FE"),
           omit = c("country", "year"))
 
-## Se på de faste effektene:
-fixef(plm.fe.time)[1:5] # Henter ut de fem første tidsfaste effektene
+## Se pÃ¥ de faste effektene:
+fixef(plm.fe.time)[1:5] # Henter ut de fem fÃ¸rste tidsfaste effektene
 
-fixef(plm.fe.ind)[1:5] # Henter ut de fem første tversnittsfaste effektene
+fixef(plm.fe.ind)[1:5] # Henter ut de fem fÃ¸rste tversnittsfaste effektene
 
-fixef(plm.fe.two)[1:5] # Henter ut de fem første tids- og tversnittssfaste effektene
+fixef(plm.fe.two)[1:5] # Henter ut de fem fÃ¸rste tids- og tversnittssfaste effektene
 
-# Signfikians på de faste effektene:
+# Signfikians pÃ¥ de faste effektene:
 summary(fixef(plm.fe.ind))
 summary(fixef(plm.fe.two, effect = "time"))
 
-# OBS: Krever mer variasjon i uavhengige variabler, ellers får vi multikolinearitet
-# Hvordan undersøke om det blir multikolinearitet?
+# OBS: Krever mer variasjon i uavhengige variabler, ellers fÃ¥r vi multikolinearitet
+# Hvordan undersÃ¸ke om det blir multikolinearitet?
 
 # Sjekker variasjon i befolkning:
 ggplot(data.complete%>% 
@@ -280,7 +280,7 @@ ggplot(data.complete%>%
   theme_bw() +
   xlab("Year") + ylab("Antall BITs")
 
-# Kan sjekke multikolineratitet med VIF, men må da ha pooled OLS
+# Kan sjekke multikolineratitet med VIF, men mÃ¥ da ha pooled OLS
 
 ols.fe.pooled <- mod1ols <- plm(data = data.complete, 
                                 fdi_inflow ~ bits + ln_gdp_pr_cap + ln_population +
@@ -291,13 +291,13 @@ ols.fe.pooled <- mod1ols <- plm(data = data.complete,
 
 car::vif(ols.fe.pooled)
 
-# Løsning ved høy vif: F. eks. ha regionvariabel eller periodevariabel.
+# LÃ¸sning ved hÃ¸y vif: F. eks. ha regionvariabel eller periodevariabel.
 
 
 #### Random effects ####
-# Mer effesiente (bruker med variasjon, drar nytte av variasjon i skjæringspunkt)
+# Mer effesiente (bruker med variasjon)
 # Antar at enhetsspesifikke konstantledd er trukket fra en tilfeldig fordeling
-# Faste effekter bør ikke korrellere med uavhengige variabler
+# Faste effekter bÃ¸r ikke korrellere med uavhengige variabler
 # Strengere antakelse: Forventer at enhetsspesifikke konstantledd er trukket fra en tilfeldig fordeling
 
 plm.re.ind <- plm(data = data.complete, 
@@ -324,15 +324,15 @@ stargazer::stargazer(plm.re.ind, plm.re.time, plm.re.two, type = "text",
 
 #### Hvilken modell? ####
 
-# Gjennom PLM kan vi kjøre en hausman test
-# Får du omtrent de samme resultatene med og uten hausman test?
-# Hvis du får omtrent samme resultater kan du bruke random effects.
+# Gjennom PLM kan vi kjÃ¸re en hausman test
+# FÃ¥r du omtrent de samme resultatene med og uten hausman test?
+# Hvis du fÃ¥r omtrent samme resultater kan du bruke random effects.
 
-# Modellene med både tverrsnitts- og tidsfaste effekter
+# Modellene med bÃ¥de tverrsnitts- og tidsfaste effekter
 phtest(plm.fe.two, plm.re.two)
 ## Hypotese: Modellene er ikke like
 ## p-verdi forteller oss at hypotesen er signifikant
-## Vi bør bruke faste effekter
+## Vi bÃ¸r bruke faste effekter
 
 # Modellene med tverssnitssfaste effekter
 phtest(plm.fe.ind, plm.re.ind)
@@ -341,10 +341,10 @@ phtest(plm.fe.ind, plm.re.ind)
 phtest(plm.fe.time, plm.re.time)
 
 
-## Mål 1: Dra nytte av så mye variasjon som mulig.
-## Mål 2: Ikke fange opp spuriøse effekter 
-# (variabler som er korrelert med uavhengig variabel og påvirker avhengig variabel)
-## Derfor: Fixed effect når det er omitted variable bias. 
+## MÃ¥l 1: Dra nytte av sÃ¥ mye variasjon som mulig.
+## MÃ¥l 2: Ikke fange opp spuriÃ¸se effekter 
+# (variabler som er korrelert med uavhengig variabel og pÃ¥virker avhengig variabel)
+## Derfor: Fixed effect nÃ¥r det er omitted variable bias. 
 
 
 #### Hvordan kommunisere? ####
@@ -352,14 +352,14 @@ phtest(plm.fe.time, plm.re.time)
 library(sjPlot)
 
 # Plotter koeffisientene og 95 % KI fra modellen med enhetsfaste effekter 
-# Lurt å standardisere koeffisientene først.
+# Lurt Ã¥ standardisere koeffisientene fÃ¸rst.
 plot_model(plm.fe.ind, sort.est = TRUE)
 
 
 #### Plotte ####
 
-# I dette eksempelet tar jeg utgangspunkt i både tids- og enhetseffekter
-# Legger til predikerte verdier på avhengig variabel for random effects i datasett
+# I dette eksempelet tar jeg utgangspunkt i bÃ¥de tids- og enhetseffekter
+# Legger til predikerte verdier pÃ¥ avhengig variabel for random effects i datasett
 data.complete$pred_fdi_inflow_re <- predict(plm.re.two)
 # Legger til predikerte verdier for pooled OLS i datasett 
 data.complete$pred_fdi_inflow_ols <- predict(mod1ols)
@@ -367,14 +367,14 @@ data.complete$pred_fdi_inflow_ols <- predict(mod1ols)
 data.complete$pred_fdi_inflow_fe <- predict(plm.fe.two)
 
 
-# Jeg gjør om en dataframe fra wide til long for å kunne få ulik farge på de predikerte verdiene i plottet
+# Jeg gjÃ¸r om en dataframe fra wide til long for Ã¥ kunne fÃ¥ ulik farge pÃ¥ de predikerte verdiene i plottet
 plot_data <- data.complete %>%
-  data.frame() %>% # Gjør om fra plm-objekt til data.frame
+  data.frame() %>% # GjÃ¸r om fra plm-objekt til data.frame
   select(country, year, fdi_inflow, pred_fdi_inflow_fe, pred_fdi_inflow_ols, pred_fdi_inflow_re) %>%  # Velger ut de aktuelle variablene
   pivot_longer(cols = contains("fdi_inflow"), 
                names_to = "model", 
                values_to = "FDI_inflow")  # omformer datasettet 
-# Argumentet cols = sier hvilke kolonner(variabler) som skal slås sammen til en kolonne
+# Argumentet cols = sier hvilke kolonner(variabler) som skal slÃ¥s sammen til en kolonne
 # contains("fdi_inflow") betyr at jeg vil ha med alle variablene som inneholder fdi_inflow
 
 head(plot_data, 10)
@@ -386,6 +386,6 @@ ggplot(plot_data %>%
   geom_line(aes(x = as.numeric(as.character(year)), y = FDI_inflow, col = model)) + # Plotter linjen
   facet_wrap(~country) +                                                            # Lager et plot per land  
   theme_classic() +
-  scale_color_discrete(labels = c("True value", "Predicted FE", "Predicted pooled OLS", "Prediced RE")) + # Setter inn labels på fargeoversikt
+  scale_color_discrete(labels = c("True value", "Predicted FE", "Predicted pooled OLS", "Prediced RE")) + # Setter inn labels pÃ¥ fargeoversikt
   xlab("Year") + ylab("FDI inflow") +
   theme(legend.position = "right", legend.title = element_blank())
